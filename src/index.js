68546368e8f03a992c084e850584bf60191df8dc
Himateja <himateja.9866@gmail.com>
2025-03-07 19:07:31 +0530

    modified the index.js

--- src/index.js
+++ src/index.js
@@ -5,42 +5,39 @@ import UserService from "../Services/service.js";
 const groq_api_key='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';
 const groqclient = new Groq({ apiKey: groq_api_key });
 
-async function getCityDetails(query=""){
-        // var cityname=city.toLowerCase();
+async function getWeatherDetails(city=""){
+        var cityname=city.toLowerCase();
         var user=new UserService();
-        var ans= user.getCityDetails(query);
+        var ans= user.getWeatherDetails(cityname);
         return ans;
     };
 
 const tools={
-    "getCityDetails":getCityDetails,
+    "getWeatherDetails":getWeatherDetails,
 }
 
 const SYSTEM_PROMPT =`
- You are an AI Assistant with START, PLAN, STATERGY, ACTION, Observation and output state
+ You are an AI Assistant with START, PLAN, ACTION, Observation and output state
  Wait for the user prompt  and first PLAN and STRATEGIZE using avaliable tools.
- After Planning, construct the new strategy with which you can perform action.
- After Strategizing,take the action with the help of strategy and appropriate tools and wait for the Observation based on the Action.
+ After Planning, Take the action with appropriate tools and wait for Observation based on the action.
  Once you get the Observations, Return the AI response based on the START prompt and Observations.
 
  Strictly follow the JSON output format as in examples
 
  Avaliable Tools:
- -functions getCityDetails(query:string):string
-    getWeatherDetails is a function that accepts a sql query and returns the recordset results in a string format
+ -functions getWeatherDetails(query:string):string
+    getWeatherDetails is a function that accepts a cityname and returns the recordset results in a string format
  -Tables 
     1)Weather_Details(city:string,temp:int) this contains information of cities and their temparature
 
  Example:
  START
  {"type": "user", "user": "What is the sum of weather of Patiala and mohali?"}
- {"type": "plan", "plan": "I will call the getCityDetails for Patiala" }
- {"type": "strategy", "strategy":"I will create a query to get the temparature of patiala from Weather_Details table"}
- {"type": "action", "function": "getCityDetails", "input": "select * from Weather_Details where city='pataila';" }
+ {"type": "plan", "plan": "I will call the getWeatherDetails for Patiala" }
+ {"type": "action", "function": "getWeatherDetails", "input": "pataila" }
  {"type": "observation", "observation": "patiala is having weather 10°C" }
  {"type": "plan", "plan": "I will call getCityDetails for Mohali" }
- {"type": "strategy", "strategy":"I will create a query to get the temparature of mohali from Weather_Details table"}
- {"type": "action", "function": "getCityDetails", "input": "select * from Weather_Details where city='mohali';" }
+ {"type": "action", "function": "getCityDetails", "input": "mohali" }
  {"type": "action", "function": "getCityDetails", "input": "mohali" }
  {"type": "observation", "observation": "mohali is having weather 14°C" }
  {"type": "output", "output": "The sum of weathers of Patiala and Mohali is 24°C" }
