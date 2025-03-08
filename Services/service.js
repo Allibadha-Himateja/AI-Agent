98aac68c30cc11671226a98e74958335a69bf5ad
Himateja <himateja.9866@gmail.com>
2025-03-08 16:02:24 +0530

    modified the services.js

--- Services/service.js
+++ Services/service.js
@@ -16,7 +16,18 @@ class UserService
 {
     
     constructor(){}
-    async getWeatherDetails(city='') {
+
+    async getSize(table="")
+    {
+        // console.log(table);
+        await sql.connect(config);
+        let request=new sql.Request();
+        let query = `SELECT COUNT(*) AS Count FROM [${table}]`;
+        let result = await request.query(query);
+        console.log(result.recordset[0].Count);
+        return `The Size of Weather_Details table is `+result.recordset[0].Count;
+    }    
+    async getCityDetails(city='') {
         let cityname = city.toLowerCase();
         await sql.connect(config);// Wait for the connection
         let request = new sql.Request();
@@ -30,14 +41,31 @@ class UserService
         }
     }
 
-    async getCitiesDetials(query="")
+    async getCitiesDetials(from_index,to_index)
     {
+        // console.log("Called"+from_index+" "+to_index);
+
         await sql.connect(config);// Wait for the connection
+
         let request = new sql.Request();
+        request.input('StartIndex',sql.Int,from_index)
+               .input('EndIndex',sql.Int,to_index);
+        var query=`WITH OrderedData AS (
+                    SELECT *, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS RowNum
+                    FROM Weather_Details
+                    )
+                SELECT * FROM OrderedData
+                WHERE RowNum BETWEEN @StartIndex AND @EndIndex;`;
         let result = await request.query(query);
-        console.log(result);
+        // console.log(result.recordset);
         if (result.recordset.length > 0) {
-            return `${result.recordset[0].city} is having weather ${result.recordset[0].temp}°C`; // Return temperature value
+            // return `${result.recordset}`;
+            var str="";
+            result.recordset.forEach((ele)=>
+                str+="city: "+ele.city+", temp: "+ele.temp+", RowNum: "+ele.RowNum+"\n"
+            );
+            console.log(str);
+            return str;
         } else {
             return 'No data found';
         }
