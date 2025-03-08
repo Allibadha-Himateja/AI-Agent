a741bbe432e6a94dfdc7f5f3ce182b57d6c6d69e
Himateja <himateja.9866@gmail.com>
2025-03-08 16:01:32 +0530

    modified the index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,36 @@
 import Groq from "groq-sdk";
 import readlineSync from 'readline-sync';
 import UserService from "../Services/service.js";
+import { groq_api_key,SYSTEM_PROMPT } from "../shared/constants.js";
 
-const groq_api_key='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';
 const groqclient = new Groq({ apiKey: groq_api_key });
 
-async function getWeatherDetails(city=""){
-        var cityname=city.toLowerCase();
-        var user=new UserService();
-        var ans= user.getWeatherDetails(cityname);
-        return ans;
-    };
-
-const tools={
-    "getWeatherDetails":getWeatherDetails,
+async function getCityDetails(city="")
+{
+    var user=new UserService();
+    var ans= user.getCityDetails(city);
+    return ans;
+};
+async function getCitiesDetails(from_index='',to_index='')
+{
+    var from=(parseInt(from_index)==0)?1:parseInt(from_index);
+    var to=(parseInt(from_index)==0)?parseInt(to_index)+1:parseInt(to_index);
+    var user=new UserService();
+    var ans=user.getCitiesDetials(from,to);
+    return ans;
+}
+async function getSize(tablename="")
+{
+    var user=new UserService();
+    var ans=user.getSize(tablename);
+    return ans;
 }
 
-const SYSTEM_PROMPT =`
- You are an AI Assistant with START, PLAN, ACTION, Observation and output state
- Wait for the user prompt  and first PLAN and STRATEGIZE using avaliable tools.
- After Planning, Take the action with appropriate tools and wait for Observation based on the action.
- Once you get the Observations, Return the AI response based on the START prompt and Observations.
-
- Strictly follow the JSON output format as in examples
-
- Avaliable Tools:
- -functions getWeatherDetails(query:string):string
-    getWeatherDetails is a function that accepts a cityname and returns the recordset results in a string format
- -Tables 
-    1)Weather_Details(city:string,temp:int) this contains information of cities and their temparature
-
- Example:
- START
- {"type": "user", "user": "What is the sum of weather of Patiala and mohali?"}
- {"type": "plan", "plan": "I will call the getWeatherDetails for Patiala" }
- {"type": "action", "function": "getWeatherDetails", "input": "pataila" }
- {"type": "observation", "observation": "patiala is having weather 10°C" }
- {"type": "plan", "plan": "I will call getCityDetails for Mohali" }
- {"type": "action", "function": "getCityDetails", "input": "mohali" }
- {"type": "action", "function": "getCityDetails", "input": "mohali" }
- {"type": "observation", "observation": "mohali is having weather 14°C" }
- {"type": "output", "output": "The sum of weathers of Patiala and Mohali is 24°C" }
-
-`;
+const tools={
+    "getCityDetails":getCityDetails,
+    "getCitiesDetails":getCitiesDetails,
+    "getSize":getSize,
+}
 
 const messages=[
     {"role":"system",content:SYSTEM_PROMPT}
@@ -50,6 +38,7 @@ const messages=[
 
 while(true)
 {
+    // this readlineSync is stoping the continuos loop 
     const query=readlineSync.question('>> ');
     const q={ type:'user', user:query };
     messages.push({"role":"user",content:JSON.stringify(q)});
@@ -65,10 +54,9 @@ while(true)
         const result=chat.choices[0].message.content;
         messages.push({role:"assistant",content:result});
 
-        // console.log("\n\n----------START AI-------------");
-        // console.log(result);
-        // console.log("----------END AI-------------\n\n");
-
+        console.log("\n\n----------START AI-------------");
+        console.log(result);
+        console.log("----------END AI-------------\n\n");
 
         const call=JSON.parse(result);
 
@@ -79,11 +67,32 @@ while(true)
         }
         else if(call.type == "action"){
             const fn = tools[call.function];
-            const observation = await fn(call.input);
-            console.log(call.input);
-            console.log(observation);
-            const obs={"type": "observation", "observation": observation };
-            messages.push({role:"assistant",content:JSON.stringify(obs)});
+            if(call.function=='getCityDetails')
+            {
+                const observation = await fn(call.input.city);
+                console.log(call.input.city);
+                console.log(observation);
+                const obs={"type": "observation", "observation": observation };
+                messages.push({role:"assistant",content:JSON.stringify(obs)});
+            }
+            if(call.function=='getCitiesDetails')
+            {
+                console.log(call.input.from_index);
+                console.log(call.input.to_index);
+                const observation=await fn(""+call.input.from_index,""+call.input.to_index);
+                console.log(observation);
+                const obs={"type": "observation", "observation": observation };
+                messages.push({role:"assistant",content:JSON.stringify(obs)});
+            }
+            if(call.function=='getSize')
+            {
+                console.log(call.input.tablename);
+                const observation = await fn(call.input.tablename);
+                console.log(observation);
+                const obs={"type": "observation", "observation": observation };
+                messages.push({role:"assistant",content:JSON.stringify(obs)});
+            }
+            
             //ikkada role lo assistant anni pettali....
         }
     }
